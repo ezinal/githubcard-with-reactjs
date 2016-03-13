e5007af727966f357e7ae09e8c0b8b9bc329697d
Hamed Esmaili <mr.hesmaili@gamil.com>
2016-03-13 07:40:45 +0330

    edit main.js file

--- statics/js/main.js
+++ statics/js/main.js
@@ -73,7 +73,10 @@ class SearchProfile extends React.Component {
 
 class Profile extends React.Component {
   render() {
-    let data = this.props.data
+    let data = this.props.data;
+    let followers = `${data.homeUrl}/followers`;
+    let repositories = `${data.homeUrl}?tab=repositories`;
+    let following = `${data.homeUrl}/following`;
     if (data.notFound === 'Not Found')
       return (
          <div className="notfound">
@@ -85,20 +88,20 @@ class Profile extends React.Component {
       return (
         <section className="github--profile">
           <div className="github--profile__info">
-            <a href={data.homeUrl} target="_blank" title={data.name}><img src={data.avatar} alt={data.username}/></a>
-            <h2><a href={data.homeUrl} title={data.username} target="_blank">{data.name}</a></h2>
-            <h3>{data.location}</h3>
+            <a href={data.homeUrl} target="_blank" title={data.name || data.username}><img src={data.avatar} alt={data.username}/></a>
+            <h2><a href={data.homeUrl} title={data.username} target="_blank">{data.name || data.username}</a></h2>
+            <h3>{data.location || 'I Live In My Mind'}</h3>
           </div>
           <div className="github--profile__state">
             <ul>
                <li>
-                  <a href="#" title="Number Of Followers"><i>{data.followers}</i><span>Followers</span></a>
+                  <a href={followers} target="_blank" title="Number Of Followers"><i>{data.followers}</i><span>Followers</span></a>
                </li>
                <li>
-                  <a href="#" title="Number Of Repositoriy"><i>{data.repos}</i><span>Repositoriy</span></a>
+                  <a href={repositories} target="_blank" title="Number Of Repositoriy"><i>{data.repos}</i><span>Repositoriy</span></a>
                </li>
                <li>
-                  <a href="#" title="Number Of Following"><i>{data.following}</i><span>Following</span></a>
+                  <a href={following} target="_blank" title="Number Of Following"><i>{data.following}</i><span>Following</span></a>
                </li>
             </ul>
           </div>
